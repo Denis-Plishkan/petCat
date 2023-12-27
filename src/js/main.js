223ed2a911a308c206622414b4b41200de6acc3f
Denis Plishkan <denchik.plishkan@gmail.com>
2023-12-27 17:15:50 +0200

    did blog block

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,6 @@
 import navbarDropdown from './modules/navbarDropdown'
 import popularCervisesSwiper from './modules/swiper'
+import answersSwiper from './modules/swiper'
+import historySwiper from './modules/swiper'
 import thisTab from './modules/thisTab'
 import auth from './modules/auth';
