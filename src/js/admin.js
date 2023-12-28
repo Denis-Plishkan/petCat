b060b3d9d48ff600817009e47012a423bdc353a4
Александр Шабашный <shabashnyioleksandr@gmail.com>
2023-12-28 19:16:45 +0200

    Отображаю сервисы

--- src/js/admin.js
+++ src/js/admin.js
@@ -1,3 +1,7 @@
+// import { auth, onAuthStateChanged } from './modules/firebase-Config';
+// import { collection, getDocs } from 'firebase/firestore/lite';
+// import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
+
 import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 import {
   auth,
@@ -68,8 +72,17 @@ document.addEventListener('DOMContentLoaded', async function () {
     let content = '';
     console.log('Hash:', window.location.hash);
     switch (hash) {
-      case '#/admin/services/':
-        content = ``;
+      case '#/admin/services':
+        content = `
+        <div class="content">
+        <h2 class="popular-services__wrapper-title">
+        Список всех услуг на данный момент
+      </h2>
+        <div class="swiper-wrapper" id="swiperWrapper"></div>
+        </div>`;
+        getDataFromFirestore();
+        break;
+
       case '#/admin/services/services-str':
         content = `
           <div class="content">
@@ -270,8 +283,68 @@ document.addEventListener('DOMContentLoaded', async function () {
 
   window.addEventListener('load', updateContent);
   window.addEventListener('hashchange', updateContent);
+
+  const createCard = (title, description) => {
+    const cardElement = document.createElement('div');
+    cardElement.classList.add('popular-services__card');
+
+    const cardTitle = document.createElement('h4');
+    cardTitle.classList.add('popular-services__card-title');
+    cardTitle.textContent = title;
+
+    const cardDescription = document.createElement('p');
+    cardDescription.textContent = description;
+
+    cardElement.appendChild(cardTitle);
+    cardElement.appendChild(cardDescription);
+
+    return cardElement;
+  };
+
+  const displayDataInHTML = (data) => {
+    const swiperWrapper = document.getElementById('swiperWrapper');
+
+    swiperWrapper.innerHTML = '';
+
+    for (let i = 0; i < data.length; i += 3) {
+      const slide = document.createElement('div');
+      slide.classList.add('swiper-slide');
+
+      const cardWrapper = document.createElement('div');
+      cardWrapper.classList.add('popular-services__card-wrapper');
+
+      for (let j = i; j < i + 3 && j < data.length; j++) {
+        const card = createCard(data[j].title, data[j].description);
+        cardWrapper.appendChild(card);
+      }
+
+      slide.appendChild(cardWrapper);
+      swiperWrapper.appendChild(slide);
+    }
+  };
+
+  const getDataFromFirestore = async () => {
+    try {
+      const querySnapshot = await getDocs(collection(db, 'services'));
+      const dataArray = [];
+
+      querySnapshot.forEach((doc) => {
+        const data = doc.data();
+        dataArray.push(data);
+        // console.log(
+        //   `ID документа: ${doc.id}, Название: ${data.title}, Описание: ${data.description}, URL изображения: ${data.imageUrl}`
+        // );
+      });
+
+      displayDataInHTML(dataArray);
+    } catch (error) {
+      console.error('Ошибка при получении данных из Firestore: ', error);
+    }
+  };
+
+  getDataFromFirestore();
 });
-//////истории
+////истории
 // import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 // import { db, collection, addDoc, storage } from './modules/firebase-Config';
 
