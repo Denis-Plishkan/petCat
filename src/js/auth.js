c60ba2df45422a7c1c68268c7a88d0dfa5543d5e
Александр Шабашный <shabashnyioleksandr@gmail.com>
2023-12-28 16:46:57 +0200

    Сообщение о неправильной почте или пароле

--- src/js/auth.js
+++ src/js/auth.js
@@ -12,8 +12,26 @@ if (authForm) {
   authForm.addEventListener('submit', async (event) => {
     event.preventDefault();
 
+    // Получаем элементы для вывода ошибок
+    const emailError = document.getElementById('emailError');
+    const passwordError = document.getElementById('passwordError');
+
+    // Сбрасываем предыдущие ошибки
+    emailError.textContent = '';
+    passwordError.textContent = '';
+
     const email = document.getElementById('loginEmail').value;
     const password = document.getElementById('loginPassword').value;
+
+    if (!email || !password) {
+      if (!email) {
+        emailError.textContent = 'Введите email';
+      }
+      if (!password) {
+        passwordError.textContent = 'Введите пароль';
+      }
+      return;
+    }
 
     try {
       const userCredential = await signInWithEmailAndPassword(
@@ -41,6 +59,19 @@ if (authForm) {
       }
     } catch (error) {
       console.error('Ошибка входа:', error.code, error.message);
+
+      if (
+        error.code === 'auth/user-not-found' ||
+        error.code === 'auth/invalid-email'
+      ) {
+        emailError.textContent = 'Неправильный email';
+        console.error('Ошибка входа: Неправильный email');
+      } else if (error.code === 'auth/wrong-password') {
+        passwordError.textContent = 'Неправильный пароль';
+        console.error('Ошибка входа: Неправильный пароль');
+      } else {
+        console.error(' Oшибка:', error);
+      }
     } finally {
       authForm.querySelector('button').disabled = false;
     }
